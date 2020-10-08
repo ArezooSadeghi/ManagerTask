41c7581eabff2844284539a3e297f41d693d12f5
arezo <arezoo.sdghi@gmail.com>
2020-10-08 09:30:50 +0330

    Initialization date and time button. Add datepicker and timepicker fragment.

--- app/src/main/java/com/example/managertask/fragment/DialogAddNewTaskFragment.java
+++ app/src/main/java/com/example/managertask/fragment/DialogAddNewTaskFragment.java
@@ -13,16 +13,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.managertask.R;
-import com.example.managertask.adapter.TaskAdapter;
 import com.example.managertask.database.TaskDatabase;
 import com.example.managertask.model.Task;
 
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DialogAddNewTaskFragment extends DialogFragment {
-    private Button mButtonSave;
+    private Button mButtonSave, mButtonDate, mButtonTime;
     private EditText mEditTextTitle, mEditTextDescription;
     private TaskDatabase mDatabase;
+    public static final String TAG1 = "DANTF";
+    public static final String TAG2 = "DANTF";
 
     public DialogAddNewTaskFragment() {
     }
@@ -45,6 +49,21 @@ public class DialogAddNewTaskFragment extends DialogFragment {
         LayoutInflater inflater = LayoutInflater.from(getActivity());
         View view = inflater.inflate(R.layout.fragment_dialog_add_new_task, null);
         findViews(view);
+        setListeners();
+        initViews();
+        AlertDialog dialog = new AlertDialog.Builder(getActivity()).setView(view).create();
+        return dialog;
+    }
+
+    private void findViews(View view) {
+        mButtonSave = view.findViewById(R.id.btn_save);
+        mButtonDate = view.findViewById(R.id.btn_date);
+        mButtonTime = view.findViewById(R.id.btn_time);
+        mEditTextTitle = view.findViewById(R.id.edittext_title);
+        mEditTextDescription = view.findViewById(R.id.edittext_description);
+    }
+
+    private void setListeners() {
         mButtonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,13 +75,30 @@ public class DialogAddNewTaskFragment extends DialogFragment {
                 dismiss();
             }
         });
-        AlertDialog dialog = new AlertDialog.Builder(getActivity()).setView(view).create();
-        return dialog;
+
+        mButtonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerFragment datePickerFragment = DatePickerFragment.newInstance();
+                datePickerFragment.show(getActivity().getSupportFragmentManager(), TAG1);
+            }
+        });
+
+        mButtonTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerFragment timePickerFragment = TimePickerFragment.newInstance();
+                timePickerFragment.show(getActivity().getSupportFragmentManager(), TAG2);
+            }
+        });
     }
 
-    private void findViews(View view) {
-        mButtonSave = view.findViewById(R.id.btn_save);
-        mEditTextTitle = view.findViewById(R.id.edittext_title);
-        mEditTextDescription = view.findViewById(R.id.edittext_description);
+    private void initViews() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        mButtonDate.setText(dateFormat.format(date));
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        mButtonTime.setText(simpleDateFormat.format(calendar.getTime()));
     }
 }
